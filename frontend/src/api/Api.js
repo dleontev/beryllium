513839b1e4d282cf35d95c0681d58f8ded7a5811
Eduard Ordukhanov <eduard.ordukhanov@gmail.com>
2018-01-16 18:51:10 -0800

    added ability to specify custom callbacks in  Api.get and Api.post calls

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -3,11 +3,14 @@ import axios from "axios";
 const API = "http://localhost:4000/api"; // process.env.REACT_APP_API_URL;
 
 export default {
-  get(url) {
+  get(url, callback = (res) =>{console.log(res)}) {
     return axios({
       method: "GET",
       headers: { "Content-Type": "application/json" },
       url: `${API}${url}`
+    })
+    .then(response => {
+      callback(response);
     })
     .catch(function (error) {
       if (error.response) {
@@ -29,7 +32,7 @@ export default {
     });
   },
 
-  post(url, data) {
+  post(url, data, callback = (res) => {console.log(res)}) {
     return axios({
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -37,7 +40,7 @@ export default {
       data: data
     })
       .then(response => {
-        console.log(response);
+        callback(response);
       })
       .catch(function (error) {
         if (error.response) {

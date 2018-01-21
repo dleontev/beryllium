2eff7de8204613cc252ff1a9cdbf0b7cc832e333
Dimitriy Leontev <dv.leontev@gmail.com>
2018-01-21 06:46:52 -0800

    Added basic assignment info render.

--- frontend/src/components/AssignmentCard.js
+++ frontend/src/components/AssignmentCard.js
@@ -3,13 +3,9 @@ import React from "react";
 class AssignmentCard extends React.Component {
   render() {
     return (
-        <tr>
-        <td>
-{this.props.id}
-        </td>
-        <td>
-          {this.props.name} 
-        </td>
+      <tr>
+        <td>{this.props.id}</td>
+        <td>{this.props.name}</td>
       </tr>
     );
   }

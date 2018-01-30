d4197bdb927c487ddb71e82bda550a5684c7b286
Dimitriy Leontev <dv.leontev@gmail.com>
2018-01-30 00:51:41 -0800

    Added a placeholder for New group.

---
+++ frontend/src/components/Form/AddGroup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AddGroup extends React.Component {
+  render() {
+    return (
+      <div>
+        <p className="title is-5">Add group.</p>
+      </div>
+    );
+  }
+}
+
+export default AddGroup;

f8bc7ba44d2ca608db9ce09440bc25e746055ba0
Dimitriy Leontev <dv.leontev@gmail.com>
2018-02-23 21:52:40 -0800

    Font awesome files are now stored locally.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
+import "./styles/bulma.css";
+import "./styles/font-awesome.min.css";
 import App from "./containers/App";
 import registerServiceWorker from "./workers/registerServiceWorker";
 

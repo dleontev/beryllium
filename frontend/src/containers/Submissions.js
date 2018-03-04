a2e3692c3c7f27b7d1ad1cbbf47f2276cc7b9b10
Eduard Ordukhanov <eduard.ordukhanov@gmail.com>
2018-03-04 03:20:29 -0800

    Created component for displayed all submissions for a given assignment

---
+++ frontend/src/containers/Submissions.js
@@ -0,0 +1,22 @@
+import React from "react";
+import api from "../api/Api";
+import PropTypes from "prop-types";
+
+class Submissions extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        This is the submissions page...
+      </div>
+    );
+  }
+}
+
+export default Submissions;
